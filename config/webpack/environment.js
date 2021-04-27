07d88c4e6a1a65a3f4d0a2bdb9783eeca1344861
Areez Khan <areezk25@gmail.com>
2021-04-27 16:45:03 +0500

    Added my action cable first project to GIT

---
+++ config/webpack/environment.js
@@ -0,0 +1,11 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jQuery/src/jquery',
+        jQuery: 'jQuery/src/jquery'
+    })
+)
+
+module.exports = environment
